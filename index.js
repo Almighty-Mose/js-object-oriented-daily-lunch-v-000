0d1aa6ab22f3cefd2f5f71db6e6aaf08eb545c23
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-22 21:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 let neighborhoodId = 0;
-let customerId = 0;
 let mealId = 0;
+let customerId = 0;
 let deliveryId = 0;
